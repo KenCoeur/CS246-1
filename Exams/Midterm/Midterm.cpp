57ec0e44c695a5b2660dc061591465e6e9e7c874
ReidConsole <jermainereid@mec.science>
2020-04-02 13:53:39 -0400

    Exams Mod

---
+++ Exams/Midterm/Midterm.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Array.h"
+#include "Node.h"
+#include "Iterator.h"
+#include "List.h"
+#include "Stack.h"
+#include "Queue.h"
+#include "Set.h"
+#include "Deque.h"
+#include "Helper.h" //Helper Functions for Testing Your Functions
+
+template <typename T>
+int LastOccurrence(ds::da::Vector<T>& data,const T& value)
+{
+    //Rewrite Definition of LastOccurrence() Here
+    return -1;
+}
+
+template <typename T>
+void BubbleSort(ds::dn::Node<T>* root)
+{
+    //Rewrite Definition of BubbleSort() Here
+    return;
+}
+
+template <typename T>
+bool HasCycle(ds::sn::Node<T>* root)
+{
+    //Rewrite Definition of HasCycle() Here 
+    return false;
+}
+
+std::string AdjacentDuplicatesRemoval(std::string str)
+{
+    //Rewrite Definition of AdjacentDuplicatesRemoval() Here
+    return "";
+}
+
+int MaxKSum(ds::da::Vector<int>& data,int k)
+{
+    //Rewrite Definition of MaxKSum() Here
+    return 0;
+}
+
+int main()
+{
+    srand(time(NULL));
+    //Test Your Functions Here
+
+    return 0;
+}
