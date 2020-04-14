f0353fe9bf15c3b97ad865cfff17439a48524697
ReidConsole <jermainereid@mec.science>
2020-04-14 15:41:20 -0400

    Labs Mod

---
+++ Labs/Lab18/lab18.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include "Array.h"
+#include "Node.h"
+#include "Iterator.h"
+#include "List.h"
+#include "Queue.h"
+#include "Set.h"
+#include "Stack.h"
+#include "Deque.h"
+
+int ToInteger(std::string num)
+{
+	return -2147483648;
+}
+
+std::string ABStringWithExceptions(int m,int n)
+{
+	return "";
+}
+
+int main()
+{
+	return 0;
+}
